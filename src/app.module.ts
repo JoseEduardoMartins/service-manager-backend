82e508b0cd0c158a5f23a2d96c25891d407ca5c5
Eduardo <m4rt1ns.jose@gmail.com>
2024-07-10 16:56:58 -0300

    configured database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,31 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 //CONFIG
 import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env'],
       isGlobal: true,
-      load: [appConfig],
+      load: [appConfig, databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: configService.get<string>('database.type'),
+          host: configService.get<string>('database.host'),
+          port: configService.get<number>('database.port'),
+          database: configService.get<string>('database.name'),
+          username: configService.get<string>('database.username'),
+          password: configService.get<string>('database.userpassword'),
+          entities: ['./**/*.entity{ .ts,.js}'],
+          synchronize: true,
+        }) as TypeOrmModuleOptions,
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
