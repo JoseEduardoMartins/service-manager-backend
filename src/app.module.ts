5251c3ca961a0d1330f83f4946e60f88dbf71b3c
Eduardo <m4rt1ns.jose@gmail.com>
2024-07-10 16:51:30 -0300

    configured environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+//CONFIG
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      load: [appConfig],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
